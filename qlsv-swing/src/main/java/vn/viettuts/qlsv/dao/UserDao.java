200478c4db1633e3e5119b0274a6beacfdbda31a
tranduong2003 <sinhviendaihoc2k3@gmail.com>
2023-05-05 21:04:58 +0700

    a

--- qlsv-swing/src/main/java/vn/viettuts/qlsv/dao/UserDao.java
+++ qlsv-swing/src/main/java/vn/viettuts/qlsv/dao/UserDao.java
@@ -5,8 +5,8 @@ import vn.viettuts.qlsv.entity.User;
 public class UserDao {
     public boolean checkUser(User user) {
         if (user != null) {
-            if ("".equals(user.getUserName()) 
-                    && "".equals(user.getPassword())) {
+            if ("admin".equals(user.getUserName()) 
+                    && "admin".equals(user.getPassword())) {
                 return true;
             }
         }
